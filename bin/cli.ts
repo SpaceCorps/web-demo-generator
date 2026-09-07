1e438a0eb2ac31b4de39db6b29097e35435559d6
rorychatt <pavel.rinne@gmail.com>
2026-09-07 17:59:02 +0200

    Support CLI prompt input from stdin or prompt file (00099)

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import { runCli } from '../src/core/cli.js';
 
-runCli(process.argv.slice(2))
+runCli(process.argv.slice(2), {
+  isTTY: process.stdin.isTTY ?? false,
+})
   .then((exitCode) => {
     process.exit(exitCode);
   })
